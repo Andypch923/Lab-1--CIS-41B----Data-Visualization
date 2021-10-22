3956d33d24e98ee78a0ce24ea6cfe940b52f4df0
Andypch923 <andypoon923@gmail.com>
2021-10-22 08:54:43 +0800

    Update test.py

--- src/test.py
+++
@@ -1,7 +0,0 @@
-import myParser
-import temperature
-import DataBase
-rawParsedTemp = myParser.myParser("Temperature.html")
-tempObj = temperature.temperature(rawParsedTemp.getRawData())
-
-dataBaseObj = DataBase.DataBase(tempObj.getTempData())
